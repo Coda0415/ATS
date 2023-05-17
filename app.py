f4c60f6b1e68bc03b74d477fefbac7056956c979
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-17 00:18:19 -0500

    check computer username, run accordingly

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import getpass
 from main import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Get the username of the computer
+username = getpass.getuser()
+
+
+if username == "colindavis":
+    if __name__ == '__main__':
+        app.run(debug=True)
+else:
+    if __name__ == "__main__":
+        app.run(host="192.168.1.55", port=8008, debug=True)
