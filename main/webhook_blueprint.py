b3b537e7c0979d22d8cedd8852e93d67f1fd8ed9
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-18 13:13:06 -0500

    keep testing

--- main/webhook_blueprint.py
+++ main/webhook_blueprint.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, Blueprint, request, jsonify, render_template
 import requests
 
@@ -6,16 +7,19 @@ webhook_log = []  # A list to store the webhook payloads
 portal_id = '9145139'
 private_app_access_token = 'pat-na1-12bad899-4b41-48a4-b609-f6ea32f91a68'
 
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
 @webhook_blueprint.route('/webhook/hubspot', methods=['POST'])
 def handle_hubspot_webhook():
     payload = request.json
-    print('Received webhook payload:', payload)
+    logging.debug('Received webhook payload: %s', payload)
 
     # Add the webhook payload to the log
     webhook_log.append(payload)
 
     # Print the current webhook log
-    print('Current webhook log:', webhook_log)
+    logging.debug('Current webhook log: %s', webhook_log)
 
     return '', 200
 
@@ -30,13 +34,13 @@ def webhook():
         if isinstance(payload, dict):
             contact_id = payload.get('objectId')
             if contact_id:
-                print(f"Retrieving contact information for contact ID: {contact_id}")
+                logging.debug(f"Retrieving contact information for contact ID: {contact_id}")
                 contact_data = get_contact_info(private_app_access_token, contact_id)
                 if contact_data:
-                    print(f"Contact information retrieved for contact ID: {contact_id}")
+                    logging.debug(f"Contact information retrieved for contact ID: {contact_id}")
                     contacts.append(contact_data)
                 else:
-                    print(f"Failed to retrieve contact information for contact ID: {contact_id}")
+                    logging.debug(f"Failed to retrieve contact information for contact ID: {contact_id}")
 
     return render_template('webhook_log.html', contacts=contacts)
 
@@ -53,7 +57,7 @@ def get_contact_info(private_app_access_token, contact_id):
         contact_data = response.json()
         return contact_data
     else:
-        print(f"Failed to retrieve contact information. Status code: {response.status_code}")
+        logging.debug(f"Failed to retrieve contact information. Status code: {response.status_code}")
         return None
 
 # Example usage
