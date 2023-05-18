e987c531d85133a06455aa39258cce3d50dd6223
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-18 07:53:19 -0500

    Added webhook html to log and test

--- main/webhook_blueprint.py
+++ main/webhook_blueprint.py
@@ -1,11 +1,22 @@
-from flask import Flask, Blueprint, request
+from flask import Flask, Blueprint, request, jsonify, render_template
 
 webhook_blueprint = Blueprint('webhook', __name__)
+webhook_log = []  # A list to store the webhook payloads
+
 @webhook_blueprint.route('/webhook/hubspot', methods=['POST'])
 def handle_hubspot_webhook():
     payload = request.json
     print('Received webhook payload:', payload)
 
-    print(payload)
+    # Add the webhook payload to the log
+    webhook_log.append(payload)
 
     return '', 200
+
+@webhook_blueprint.route('/webhook/log', methods=['GET'])
+def get_webhook_log():
+    return jsonify(webhook_log)
+
+@webhook_blueprint.route('/webhook')
+def webhook():
+    return render_template('webhook_log.html')
