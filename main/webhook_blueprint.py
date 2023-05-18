e6e1d445195c0619844469af2f0ba5b5a2377c05
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-18 10:44:31 -0500

    convert to string

--- main/webhook_blueprint.py
+++ main/webhook_blueprint.py
@@ -23,12 +23,13 @@ def get_webhook_log():
 def webhook():
     contacts = []
     for payload in webhook_log:
-        contact_id = payload['objectId']  # Access 'objectId' directly from the payload
+        contact_id = str(payload['objectId'])  # Convert contact_id to string
         if contact_id:
             contact = get_contact_info(contact_id)
             contacts.append(contact)
 
     return render_template('webhook_log.html', contacts=contacts)
+
 
 def get_contact_info(contact_id):
     headers = {
