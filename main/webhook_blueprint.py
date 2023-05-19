4dc48f8b355967211b8b2467dfbbff6b7fdb73c0
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-19 10:19:57 -0500

    changed json stuff

--- main/webhook_blueprint.py
+++ main/webhook_blueprint.py
@@ -6,23 +6,25 @@ from hubspot.crm.contacts import ApiException
 webhook_blueprint = Blueprint('webhook', __name__)
 private_app_access_token = 'pat-na1-12bad899-4b41-48a4-b609-f6ea32f91a68'
 
-
 # Create an instance of the API client
-api_client = hubspot.Client(access_token=private_app_access_token)
+api_client = hubspot.Client.create_with_api_key(private_app_access_token)
 
-
-@webhook_blueprint.route('/webhook/hubspot', methods=['POST', 'GET'])
+@webhook_blueprint.route('/webhook/hubspot', methods=['POST'])
 def webhook():
-    payload = request.json
-    if isinstance(payload, list):
-        # Handle a list of payloads
-        if len(payload) > 0:
-            contact_id = payload[0].get('objectId')
+    try:
+        payload = request.get_json()
+        if isinstance(payload, list):
+            # Handle a list of payloads
+            if len(payload) > 0:
+                contact_id = payload[0].get('objectId')
+                # You can now use the contact_id variable in your further processing
+                app.logger.info('Received payload. Contact ID: %s', contact_id)
+        else:
+            # Handle a single payload
+            contact_id = payload.get('objectId')
             # You can now use the contact_id variable in your further processing
-            print('Received payload. Contact ID:', contact_id)
-    else:
-        # Handle a single payload
-        contact_id = payload.get('objectId')
-        # You can now use the contact_id variable in your further processing
-        print('Received payload. Contact ID:', contact_id)
-    return 'Success'
+            app.logger.info('Received payload. Contact ID: %s', contact_id)
+        return 'Success'
+    except Exception as e:
+        app.logger.error('Error processing webhook payload: %s', str(e))
+        return 'Error', 500
