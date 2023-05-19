e8d12fd5b3bde543386a27513071b436a0675d5c
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-19 09:26:31 -0500

    payload dict

--- main/webhook_blueprint.py
+++ main/webhook_blueprint.py
@@ -6,37 +6,35 @@ from hubspot.crm.contacts import ApiException
 webhook_blueprint = Blueprint('webhook', __name__)
 private_app_access_token = 'pat-na1-12bad899-4b41-48a4-b609-f6ea32f91a68'
 
+
 # Create an instance of the API client
 api_client = hubspot.Client(access_token=private_app_access_token)
 
 
-@webhook_blueprint.route('/webhook/hubspot', methods=['POST', 'GET'])
+@webhook_blueprint.route('/webhook/hubspot', methods=['POST'])
 def handle_hubspot_webhook():
     payload = request.json
     logging.debug('Received webhook payload: %s', payload)
 
-    # Extract the contact ID from the payload
-    contact_id = payload['objectId']
+    if isinstance(payload, dict):
+        # Extract the contact ID from the payload
+        contact_id = payload.get('objectId')
 
-    try:
-        # Fetch the contact by ID
-        contact_fetched = api_client.crm.contacts.basic_api.get_by_id(contact_id,
-                                                                      properties=["firstname", "lastname", "phone",
-                                                                                  "appstatus",
-                                                                                  "best_way_to_contact_you_",
-                                                                                  "drugtestresult",
-                                                                                  "hs_marketable_reason_id"])
+        try:
+            # Fetch the contact by ID
+            contact_fetched = api_client.crm.contacts.basic_api.get_by_id(contact_id,
+                                                                          properties=["firstname", "lastname", "phone",
+                                                                                      "appstatus",
+                                                                                      "best_way_to_contact_you_",
+                                                                                      "drugtestresult",
+                                                                                      "hs_marketable_reason_id"])
 
-        # Render the HTML template with contact information
-        return render_template('webhook_log.html', contact=contact_fetched)
+            # Render the HTML template with contact information
+            return render_template('webhook_log.html', contact=contact_fetched)
 
-    except ApiException as e:
-        print("Exception when requesting contact by ID: %s\n" % e)
-
-    # Add the webhook payload to the log
-    webhook_log.append(payload)
-
-    # Print the current webhook log
-    logging.debug('Current webhook log: %s', webhook_log)
+        except ApiException as e:
+            print("Exception when requesting contact by ID: %s\n" % e)
+    else:
+        print("Invalid payload format")
 
     return '', 200
