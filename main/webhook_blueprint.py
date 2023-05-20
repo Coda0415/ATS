bae59eee33d4331895b666ee128b211d7994b217
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-19 21:25:26 -0500

    and again over forever

--- main/webhook_blueprint.py
+++ main/webhook_blueprint.py
@@ -4,14 +4,16 @@ import json
 
 webhook_blueprint = Blueprint('webhook', __name__)
 data_storage = {}
-object_id = {}
 
 @webhook_blueprint.route('/webhook/hubspot', methods=['POST'])
 def hubspot_webhook():
+    global data_storage
     data = json.loads(request.data)
     object_id = data[0].get('objectId', None)  # extract the objectId from the first dictionary in the list
+    data_storage = object_id
     print(f"Received object ID: {object_id}")
     return jsonify({"status":"ok"}), 200
+
 @webhook_blueprint.route('/webhook/data')
 def display_data():
-    return render_template('webhook_log.html', data=object_id)
+    return render_template('webhook_log.html', data=data_storage)
