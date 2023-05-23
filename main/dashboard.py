4e3e4800a0a0c27168912ecd1eddb3c6099b6998
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-23 15:31:25 -0500

    changed the dashboard, fixed the modal.

--- main/dashboard.py
+++ main/dashboard.py
@@ -1,20 +1,26 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
-from .models import db, jobmasterlist, accountmanagermasterlist, employeemasterlist, openpositionsroster, applicants as Applicant
+from .models import jobmasterlist, accountmanagermasterlist, employeemasterlist, openpositionsroster, applicants as Applicant
 
 dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 
 
 def get_manager_jobs(manager_name):
     jobs = jobmasterlist.query.filter(jobmasterlist.accountmanager.contains(manager_name)).all()
-    # print(f"Jobs for manager {manager_name}: {jobs}")
     return jobs
 
 
 def get_open_positions(manager_name):
     open_positions = openpositionsroster.query.filter_by(accountmanager=manager_name).all()
-    # print(f"Open positions for manager {manager_name}: {open_positions}")
     return open_positions
+
+
+def get_user_accounts():
+    if current_user.is_authenticated and current_user.role == 'AccountManager':
+        manager = accountmanagermasterlist.query.filter_by(manageremail=current_user.email).first()
+        if manager:
+            return jobmasterlist.query.filter(jobmasterlist.accountmanager.contains(manager.accountmanager)).all()
+    return []
 
 
 @dashboard.route('/dashboard', methods=['GET'])
@@ -25,40 +31,44 @@ def dashboard_view():
             if manager:
                 manager_name = manager.accountmanager
                 jobs = get_manager_jobs(manager_name)
-                open_positions = get_open_positions(manager_name)
-                return render_template('dashboard.html', jobs=jobs, open_positions=open_positions)
+                accounts = get_user_accounts()
+                return render_template('dashboard.html', jobs=jobs, accounts=accounts, selected_menu='employees')
     return render_template('login.html')
 
 
-
-@dashboard.route('/open_positions')
-def open_positions():
-    manager = accountmanagermasterlist.query.filter_by(manageremail=current_user.email).first()
-    if manager:
-        manager_name = manager.accountmanager
-        open_positions = get_open_positions(manager_name)
-    return render_template('open_positions.html', open_positions=open_positions)
+@dashboard.route('/dashboard/<menu>', methods=['GET'])
+def dashboard_view_menu(menu):
+    if current_user.is_authenticated:
+        if current_user.role == 'AccountManager':
+            manager = accountmanagermasterlist.query.filter_by(manageremail=current_user.email).first()
+            if manager:
+                manager_name = manager.accountmanager
+                jobs = get_manager_jobs(manager_name)
+                accounts = get_user_accounts()
+                if menu == 'employees':
+                    return render_template('dashboard.html', jobs=jobs, accounts=accounts, selected_menu='employees')
+                elif menu == 'open_positions':
+                    open_positions = get_open_positions(manager_name)
+                    return render_template('dashboard.html', jobs=jobs, accounts=accounts, open_positions=open_positions, selected_menu='open_positions')
+                elif menu == 'applicants':
+                    applicants = Applicant.query.all()
+                    return render_template('dashboard.html', jobs=jobs, accounts=accounts, all_applicants=applicants, selected_menu='applicants')
+    return render_template('login.html')
 
 
 @dashboard.route('/get_employees/<job_name>/<job_number>')
-def get_employees(job_name,job_number):
+def get_employees(job_name, job_number):
     # Get all the employees from employeemasterlist that have the specified job name
     employees = employeemasterlist.query.filter_by(jobname=job_name).all()
 
-    # Get the jobs and open positions for the current manager
+    # Get the jobs for the current manager
     manager = accountmanagermasterlist.query.filter_by(manageremail=current_user.email).first()
     if manager:
         manager_name = manager.accountmanager
         jobs = get_manager_jobs(manager_name)
-        open_positions = get_open_positions(manager_name)
+        accounts = get_user_accounts()
 
-        # Retrieve applicants for the selected job
-        applicants = Applicant.query.filter_by(jobnumber=job_number).all()
-
-
-        return render_template('dashboard.html', employees=employees, open_positions=open_positions, jobs=jobs, job_name=job_name, applicants=applicants)
-
-
+        return render_template('dashboard.html', employees=employees, jobs=jobs, accounts=accounts, job_name=job_name, selected_menu='employees')
 
 
 @dashboard.route('/dashboard/open_positions')
@@ -67,9 +77,12 @@ def dashboard_open_positions():
         manager = accountmanagermasterlist.query.filter_by(manageremail=current_user.email).first()
         if manager:
             manager_name = manager.accountmanager
-            open_positions = openpositionsroster.query.filter_by(accountmanager=manager_name).all()
-            return render_template('open_positions.html', open_positions=open_positions)
+            open_positions = get_open_positions(manager_name)
+            jobs = get_manager_jobs(manager_name)
+            accounts = get_user_accounts()
+            return render_template('dashboard.html', open_positions=open_positions, jobs=jobs, accounts=accounts, selected_menu='open_positions')
     return render_template('login.html')
+
 
 @dashboard.route('/dashboard/open_positions/<job_name>')
 def open_positions_by_job(job_name):
@@ -78,8 +91,7 @@ def open_positions_by_job(job_name):
         if manager:
             manager_name = manager.accountmanager
             open_positions = openpositionsroster.query.filter_by(accountmanager=manager_name, jobdescription=job_name).all()
-            jobs = get_manager_jobs(manager_name)  # Add this line to get jobs for the current manager
-            return render_template('dashboard.html', open_positions=open_positions, job_name=job_name, jobs=jobs)
+            jobs = get_manager_jobs(manager_name)
+            accounts = get_user_accounts()
+            return render_template('dashboard.html', open_positions=open_positions, job_name=job_name, jobs=jobs, accounts=accounts, selected_menu='open_positions')
     return render_template('login.html')
-
-
