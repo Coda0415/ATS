2dfe629cb52940156f0bedcdfe3e6db96ae405a4
Coda0415 <42657248+Coda0415@users.noreply.github.com>
2023-05-23 07:41:21 -0500

    close modal edit

--- main/tester.py
+++ main/tester.py
@@ -1,16 +1,32 @@
-import hubspot
-from hubspot.crm.contacts import ApiException
+import requests
 
+# Set up authentication
 private_app_access_token = 'pat-na1-12bad899-4b41-48a4-b609-f6ea32f91a68'
+base_url = 'https://api.hubapi.com'
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {private_app_access_token}'
+}
 
+# Retrieve last engagement for a contact
+contact_id = '1580001'
 
+# Retrieve all engagements for the contact
+engagements_url = f'{base_url}/engagements/v1/engagements/associated/contact/{contact_id}/paged'
 
-# Create an instance of the API client
-api_client = hubspot.Client(access_token=private_app_access_token)
+response = requests.get(engagements_url, headers=headers)
+if response.status_code == 200:
+    engagements = response.json().get('results', [])
+    if engagements:
+        last_engagement = engagements[1]
+        engagement_type = last_engagement['engagement']['type']
+        print(f"Last contact type: {engagement_type}")
 
-try:
-    # Fetch the contact by ID
-    contact_fetched = api_client.crm.contacts.basic_api.get_by_id('1580051', properties=["firstname", "lastname", "phone", "appstatus","best_way_to_contact_you_","drugtestresult","hs_marketable_reason_id"])
-    print(contact_fetched)
-except ApiException as e:
-    print("Exception when requesting contact by ID: %s\n" % e)
+        metadata = last_engagement.get('metadata', {})
+        print("Metadata:")
+        for key, value in metadata.items():
+            print(f"{key}: {value}")
+    else:
+        print("No engagements found for the contact.")
+else:
+    print("Failed to retrieve engagements.")
